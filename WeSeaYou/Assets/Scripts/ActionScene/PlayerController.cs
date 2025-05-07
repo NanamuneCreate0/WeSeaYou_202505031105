8d55c8111bef8421abc7d95faa2b43220dbd81b3
NanamuneCreate0 <nanamuneCreate0@gmail.com>
2025-05-07 09:10:42 +0900

    Revert "Merge remote-tracking branch 'origin/Nanamune' into StageMake"
    This reverts commit 540d0879f67c0c413c73085cdfdc975d80d4d7d2, reversing
    changes made to a08db8081937455cbe2570e943c25ecf92c2aac3.

--- WeSeaYou/Assets/Scripts/ActionScene/PlayerController.cs
+++ WeSeaYou/Assets/Scripts/ActionScene/PlayerController.cs
@@ -46,8 +46,5 @@ public class PlayerController : MonoBehaviour
             HandlingPlayer.transform.Translate(new Vector3(Time.deltaTime * -PlayerSpeed, 0, 0));
         }
 
-        //“–‚½‚è”»’è‚¯‚·
-        //
-
     }
 }
