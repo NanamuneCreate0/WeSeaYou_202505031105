144f1bcba636a26cdf98aca30107e2f2c1c350a4
NanamuneCreate0 <nanamuneCreate0@gmail.com>
2025-05-16 16:41:39 +0900

    アイテムを拾う仕様を作る途中

--- WeSeaYou/Assets/Scripts/ActionScene/NanamuneOnly/ItemDisplayer.cs
+++ WeSeaYou/Assets/Scripts/ActionScene/NanamuneOnly/ItemDisplayer.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class ItemDisplayer : MonoBehaviour
 {
@@ -17,16 +19,60 @@ public class ItemDisplayer : MonoBehaviour
     public void SetItemDisplay()
     {
         Debug.Log("set");
+        StartCoroutine("ChangeColor");
 
-        /*foreach (Transform tf in transform)
+        /*
+        //ÉZÉãÇÃêîÇçáÇÌÇπÇÈ
+        if(PublicStaticStatus.ItemList.Count !=transform.childCount)
         {
-            GameObject.Destroy(tf.gameObject);
+            Debug.Log(PublicStaticStatus.ItemList.Count);
+            for (int i = 0; i < 30; i++)
+            {
+                if (PublicStaticStatus.ItemList.Count < transform.childCount)
+                {
+                    Instantiate(MyItemDisplayerCell, transform);
+                }
+                if (PublicStaticStatus.ItemList.Count > transform.childCount)
+                {
+                    Destroy(transform.GetChild(0).gameObject);
+                }
+            }
+            
+        }
+
+        for (int i = 0; i < PublicStaticStatus.ItemList.Count; ++i)
+        {
+            Debug.Log(PublicStaticStatus.ItemList[i]);
+            transform.GetChild(i).GetComponent<Image>().sprite = PublicStaticStatus.ItemList[i].sprite;
         }*/
-        foreach (Item item in PublicStaticStatus.ItemList)
+    }
+
+    IEnumerator ChangeColor()
+    {
+
+        //ÉZÉãÇÃêîÇçáÇÌÇπÇÈ
+        if (PublicStaticStatus.ItemList.Count != transform.childCount)
         {
-            Debug.Log(item);
-            Instantiate(MyItemDisplayerCell, Vector3.zero, Quaternion.identity, transform);
-            MyItemDisplayerCell.GetComponent<Image>().sprite = item.sprite;
+            Debug.Log(PublicStaticStatus.ItemList.Count);
+            Debug.Log(transform.childCount);
+            for (int i = 0; i < 30; i++)
+            {
+                if (PublicStaticStatus.ItemList.Count > transform.childCount)
+                {
+                    Instantiate(MyItemDisplayerCell, transform);
+                }
+                if (PublicStaticStatus.ItemList.Count < transform.childCount)
+                {
+                    Destroy(transform.GetChild(0).gameObject);
+                }
+            }
+
+        }
+        yield return null;
+        for (int i = 0; i < PublicStaticStatus.ItemList.Count; ++i)
+        {
+            Debug.Log(PublicStaticStatus.ItemList[i]);
+            transform.GetChild(i).GetComponent<Image>().sprite = PublicStaticStatus.ItemList[i].sprite;
         }
     }
 }
