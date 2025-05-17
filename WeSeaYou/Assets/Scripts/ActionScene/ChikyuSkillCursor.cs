ed40a74104a86f22f509d57b69f515c3309d3025
NanamuneCreate0 <nanamuneCreate0@gmail.com>
2025-05-17 19:07:25 +0900

    アイテムUI進む

---
+++ WeSeaYou/Assets/Scripts/ActionScene/ChikyuSkillCursor.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ChikyuSkillCursor : MonoBehaviour
+{
+    public int BiginningOfHilightedRange;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
--- WeSeaYou/Assets/Scripts/ActionScene/ChikyuSkillHand.cs
+++ WeSeaYou/Assets/Scripts/ActionScene/ChikyuSkillHand.cs
@@ -1,20 +1,29 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChikyuSkillHand : MonoBehaviour
 {
     public List<Item> FirstHandItems_Test = new List<Item>();
     public List<Item> HandItems = new List<Item>();
+
+    public int BiginningOfHilightedRange=0;
+
     [SerializeField]
     List<GameObject> HandDisplayCells = new List<GameObject>();
+    [SerializeField]
+    GameObject HandDisplayCell;
 
+    const float angleDistance = 36;
     const float radius=100;
-    const float moveTime = 0.2f;
+    const float moveTime = 0.15f;
+    const float FirstOffSetAngle = 162;
 
-    float offsetAngle = 162;
+    float offSetAngle;
     float lastOffsetAngle;
-    bool isMoving=false;
+    int isMoving=0;//0:静止//1:左//2:右
     float wayToMove;
     float timer;
 
@@ -22,50 +31,157 @@ public class ChikyuSkillHand : MonoBehaviour
     {
         //テストとして10個アイテムを持つ
         foreach(var item in FirstHandItems_Test) { HandItems.Add(item); }
-        SetPos();
+        offSetAngle = FirstOffSetAngle;
+        SetCellPos();
+        SetItem(0);
     }
 
     void Update()
     {
-        if(!isMoving&&Input.GetKeyDown(KeyCode.A))
+        if (isMoving == 0 && Input.GetKeyDown(KeyCode.A))
         {
-            //offsetAngle += 36;
-            //SetPos();
-            lastOffsetAngle = offsetAngle;
-            wayToMove = +36;
-            isMoving = true;
+            GameObject go = Instantiate(HandDisplayCell, transform);
+            HandDisplayCells.Add(go);
+            offSetAngle = FirstOffSetAngle;
+            SetCellPos();
+            SetItem(+1);
+
+            //動く用意
+            lastOffsetAngle = offSetAngle;
+            wayToMove = +angleDistance;
+            isMoving = 1;
+        }
+        if (isMoving == 0 && Input.GetKeyDown(KeyCode.D))
+        {
+            GameObject go = Instantiate(HandDisplayCell, transform);
+            HandDisplayCells.Insert(0, go);//この二つ
+            offSetAngle = FirstOffSetAngle + angleDistance;//この二つが高速で処理されていい感じ
+            SetCellPos();
+            SetItem(-1);
+
+            //動く用意
+            lastOffsetAngle = offSetAngle;
+            wayToMove = -angleDistance;
+            isMoving = 2;
         }
 
-        if(isMoving)
+        if (isMoving == 1)
         {
             if (timer < moveTime)
             {
                 timer += Time.deltaTime;
-                offsetAngle = lastOffsetAngle + (wayToMove * (timer / moveTime));
-                SetPos();
+                offSetAngle = lastOffsetAngle + (wayToMove * (timer / moveTime));
+                SetCellPos();
             }
             else if (timer >= moveTime)
             {
-                isMoving = false;
+                isMoving = 0;
                 timer = 0;
-                offsetAngle =lastOffsetAngle+ wayToMove;
-                SetPos();
+                offSetAngle = lastOffsetAngle + wayToMove;
+                Destroy(HandDisplayCells[0]);
+                HandDisplayCells.RemoveAt(0);
+                offSetAngle = FirstOffSetAngle;
+                SetCellPos();
+            }
+        }
+        if (isMoving == 2)
+        {
+            if (timer < moveTime)
+            {
+                timer += Time.deltaTime;
+                offSetAngle = lastOffsetAngle + (wayToMove * (timer / moveTime));
+                SetCellPos();
+            }
+            else if (timer >= moveTime)
+            {
+                isMoving = 0;
+                timer = 0;
+                offSetAngle = lastOffsetAngle + wayToMove;
+                Destroy(HandDisplayCells[HandDisplayCells.Count - 1]);
+                HandDisplayCells.RemoveAt(HandDisplayCells.Count-1);
+                offSetAngle = FirstOffSetAngle;
+                SetCellPos();
             }
         }
     }
-    void SetPos()
+    void SetCellPos()//SetHandDisplayCellPosの省略
     {
-
-        float splitAngle = 360 / HandDisplayCells.Count;
         var rect = transform as RectTransform;
 
         for (int i = 0; i < HandDisplayCells.Count; i++)
         {
             RectTransform HandDisplayCell = HandDisplayCells[i].transform as RectTransform;
-            float currentAngle = splitAngle * -i + offsetAngle;
+            float currentAngle = angleDistance * -i + offSetAngle;
             HandDisplayCell.anchoredPosition = new Vector2(
                 Mathf.Cos(currentAngle * Mathf.Deg2Rad),
                 Mathf.Sin(currentAngle * Mathf.Deg2Rad)) * radius;
+        }
+    }
+
+    void SetItem(int num)
+    {
+        if (num == 0)
+        {
+            for (int i = 0; i < HandDisplayCells.Count; i++)
+            {
+                HandDisplayCells[i].GetComponent<Image>().sprite = HandItems[i].sprite;
+            }
+        }
+        if (num == 1)
+        {
+            BiginningOfHilightedRange++;
+            if (BiginningOfHilightedRange == HandItems.Count) BiginningOfHilightedRange=0;
+
+            for (int i = 0; i < HandDisplayCells.Count ; i++)
+            {
+                int num1;
+                num1= (i + BiginningOfHilightedRange - 1)% HandItems.Count;
+                if (num1 < 0) { num1 += HandItems.Count; }
+                //if (i + BiginningOfHilightedRange - 1 < 0) { num1 = i + BiginningOfHilightedRange - 1 + HandItems.Count; }
+                //else if(i + BiginningOfHilightedRange - 1 >= HandItems.Count) { num1= i + BiginningOfHilightedRange - 1 - HandItems.Count; }
+                //else { num1 = i + BiginningOfHilightedRange - 1; }
+                HandDisplayCells[i].GetComponent<Image>().sprite = HandItems[num1].sprite;
+                /*try
+                {
+                    HandDisplayCells[i].GetComponent<Image>().sprite = HandItems[num].sprite;
+                }
+                catch (System.ArgumentOutOfRangeException e)
+                {
+                    Debug.Log(e.Message);
+                    HandDisplayCells[i].GetComponent<Image>().sprite = HandItems[i + BiginningOfHilightedRange - 1+HandItems.Count].sprite;
+                }*/
+            }
+        }
+        if (num == -1)
+        {
+            BiginningOfHilightedRange--;
+            if (BiginningOfHilightedRange <0) BiginningOfHilightedRange+=HandItems.Count;
+            //if (BiginningOfHilightedRange ==0) BiginningOfHilightedRange=HandItems.Count-1;
+            //else if (BiginningOfHilightedRange >0) BiginningOfHilightedRange--;
+            //Debug.Log(BiginningOfHilightedRange);
+
+            for (int i = 0; i < HandDisplayCells.Count; i++)
+            {
+                int num1;
+                num1= (i + BiginningOfHilightedRange)%HandItems.Count;
+                //if (i + BiginningOfHilightedRange >= HandItems.Count) { num1 = i + BiginningOfHilightedRange - HandItems.Count; }
+                //else { num1 = i + BiginningOfHilightedRange; }
+                HandDisplayCells[i].GetComponent<Image>().sprite = HandItems[num1].sprite;
+            }
+
+            /*for (int i = 0; i < HandDisplayCells.Count ; i++)
+            {
+                //HandDisplayCells[i].GetComponent<Image>().sprite = HandItems[i+ BiginningOfHilightedRange].sprite;
+                try
+                {
+                    HandDisplayCells[i].GetComponent<Image>().sprite = HandItems[i + BiginningOfHilightedRange].sprite;
+                }
+                catch (System.ArgumentOutOfRangeException e)
+                {
+                    Debug.Log(e.Message);
+                    HandDisplayCells[i].GetComponent<Image>().sprite = HandItems[i + BiginningOfHilightedRange-HandItems.Count].sprite;
+                }
+            }*/
         }
     }
 
