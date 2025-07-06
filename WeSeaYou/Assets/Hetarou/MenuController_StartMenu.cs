e34ce89d67fa560406a9b79d6d94faa25e5d62e2
NanamuneCreate0 <nanamuneCreate0@gmail.com>
2025-07-07 00:23:56 +0900

    セーブデータロードの画面で十字操作の仕方を変える

--- WeSeaYou/Assets/Hetarou/MenuController_StartMenu.cs
+++ WeSeaYou/Assets/Hetarou/MenuController_StartMenu.cs
@@ -22,24 +22,87 @@ public class StartMenuController : MonoBehaviour
 
     public int CurrentLoadNumber;
 
-    public int a = 0;
-    public int b = 0;
-    public int[,] ChoosingNumber = new int[2, 2]
+    private int a = 1;
+    private int b = 0; 
+    private int[,] ChoosingNumber = new int[3, 2]
     {
         {0, 1},
-        {2, 3}
+        {2, 3},
+        {4, 5}
     };
 
     void Start()
     {
         //最初の選択肢に合わせる
-        ChoosingChoiceObject = ChoiceObject[0];
+        ChoosingChoiceObject = ChoiceObject[2];
         //カーソル合わせる
-        ChoosingLoadBox(0, 0);
+        ChoosingLoadBox(1, 0);
     }
 
     void Update()
     {
+        //カーソルコントローラー
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) )
+        {
+            if (a == 1)
+            {
+                //NotNullChoice(a+1,b)、a+1,bの組み合わせが侵入可能か有効か判定、有効なら処理を続ける//直感的//説明しやすい//この時例外処理…だと説明しつらい//こだわりかも
+                EndBox();
+                a = 2;
+                ChoosingLoadBox(a, b);
+            }
+            if (a == 0)
+            {
+                EndBox();
+                a = 1;
+                ChoosingLoadBox(a, b);
+            }
+        }
+
+        else if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)))
+        {
+            if (a == 2)
+            {
+                EndBox();
+                a = 1;
+                ChoosingLoadBox(a, b);
+            }
+
+            else if (a == 1)
+            {
+                if(IsNotNullChoice(a-1,b))
+                {
+                    EndBox();
+                    a = 0;
+                    ChoosingLoadBox(a, b);
+                }
+                else//×ボタンに行くため、固定で[0,1]地点に行く
+                {
+                    EndBox();
+                    a = 0;
+                    b = 1;
+                    ChoosingLoadBox(a, b);
+                }
+            }
+        }
+
+        else if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && b == 0)
+        {
+            EndBox();
+            b = 1;
+            ChoosingLoadBox(a, b);
+        }
+
+        else if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && b == 1)
+        {
+            if (IsNotNullChoice(a, b-1))
+            {
+                EndBox();
+                b = 0;
+                ChoosingLoadBox(a, b);
+            }
+        }
+        /*
         //カーソルコントローラー
         if((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && a == 0)
         {
@@ -77,12 +140,13 @@ public class StartMenuController : MonoBehaviour
             b = 0;
             ChoosingLoadBox(a, b);
         }
+        */
         //カーソル合わせる
-        if(Cursor.visible == true && Input.anyKeyDown && !Input.GetMouseButtonDown(0))
+        if (Cursor.visible == true && Input.anyKeyDown && !Input.GetMouseButtonDown(0))
         {
             Cursor.visible = false;
             EndBox();
-            a = 0;
+            a = 1;
             b = 0;
             ChoosingLoadBox(a, b);
         }
@@ -104,15 +168,23 @@ public class StartMenuController : MonoBehaviour
         }
     }
 
+    private bool IsNotNullChoice(int numA, int numB)
+    {
+        if ((numA, numB) == (0, 0)) { return false; }
+        else { return (true); }
+    }
+
     public void ChoosingLoadBox(int VerticalNumber, int HorizontalNumber)
     {
+        Debug.Log("" + a + "," + b + "," + ChoosingNumber[a, b]);
         ChoosingChoiceObject = ChoiceObject[ChoosingNumber[VerticalNumber, HorizontalNumber]];
-        White();
+        Hilight();//選択中を分かりやすくする
     }
 
     public void EndBox()
     {
-        ChoosingChoiceObject.GetComponent<Image>().color = new Color32(0, 0, 0, 125);
+        ChoosingChoiceObject.GetComponent<Image>().color = new Color32(0, 0, 0, 125);//仮。ホントはこっち→//ChoosingChoiceObject.GetComponent<Image>().color = new Color32(255, 255, 255, 125);
+
     }
 
     public void GetLoad(int VerticalNumber, int HorizontalNumber)
@@ -120,8 +192,9 @@ public class StartMenuController : MonoBehaviour
         Debug.Log(ChoosingNumber[VerticalNumber, HorizontalNumber]);
     }
 
-    public void White()
+    public void Hilight()
     {
-        ChoosingChoiceObject.GetComponent<Image>().color = new Color32(255, 255, 255, 125);
+        ChoosingChoiceObject.GetComponent<Image>().color = new Color32(255, 255, 255, 125);//仮//元の色に戻すという意味の処理
+        //白を重ね掛けできるようにしたい。//MaskかShaderを使いそう
     }
 }
