870cca4c000170519098f05cca120ffb3ad4e399
Hetarou <isida0427@gmail.com>
2026-03-29 15:35:31 +0900

    仮の角パワーを制作しました。 Assets>Scripts>ActionScene>Hetarou_Testsの中にスクリプトが入っています。

--- WeSeaYou/Assets/Scripts/Scenario/Tests/ScaleTest.cs
+++ WeSeaYou/Assets/Scripts/Scenario/Tests/ScaleTest.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
-using DG.Tweening; // © ‚±‚ê‚ªÔ”gü‚É‚È‚Á‚Ä‚¢‚È‚¯‚ê‚Î¬Œ÷I
+using DG.Tweening;
+using System.Collections; // © ‚±‚ê‚ªÔ”gü‚É‚È‚Á‚Ä‚¢‚È‚¯‚ê‚Î¬Œ÷I
 
 public class ScaleTest : MonoBehaviour
 {
     [SerializeField] private float _targetHeight;
     [SerializeField] private float _duration;
+
+    private CanvasGroup _group;
     void Start()
     {
         // 2•b‚©‚¯‚Ä‚‚³‚ğ3‚É‚·‚é
+        _group = GetComponent<CanvasGroup>();
         transform.DOScaleY(_targetHeight, _duration).SetEase(Ease.InOutSine);
+        if(_group != null) _group.DOFade(1f, _duration).SetEase(Ease.InOutSine);
     }
+
+    
 }
