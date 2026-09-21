bb39d95b60c691a35bcf530379fd1aa523ed9c14
NanamuneCreate0 <nanamuneCreate0@gmail.com>
2026-09-21 22:11:21 +0900

    Chapterの変数設定

--- WeSeaYou/Assets/Scripts/PublicStaticStatus.cs
+++ WeSeaYou/Assets/Scripts/PublicStaticStatus.cs
@@ -1,9 +1,24 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-[System.Serializable]
-public class PublicStaticStatus : MonoBehaviour
+public static class PublicStaticStatus
 {
     public static List<ChikyuSkillItemData> ChikyuSkillItemList = new List<ChikyuSkillItemData>();
     //public static List<StageItemData> StageItemList = new List<StageItemData>();
+
+    public enum Chapter
+    {
+        None,
+        ChapterA,//1-1とか
+        ChapterB,//1-2とか
+        ChapterC,//2-1とか
+        ChapterD,//2-2とか
+        ChapterE,//2-3とか
+        ChapterF
+    }
+    public static Chapter ClearedChapter = Chapter.None;
+    /*へたちゃんはこのenumを参照して、指定のシナリオをスタートさせる
+     * ななむねは、ステージゴール時にenumを+1する
+     * はじめてのゲームスタート時、始めるボタンでNone→ChapterA
+     */
 }
