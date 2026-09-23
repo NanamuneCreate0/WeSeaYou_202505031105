ef09db319ccbe729add88aa5ee42a090378c973b
NanamuneCreate0 <nanamuneCreate0@gmail.com>
2026-09-23 20:16:31 +0900

    土埃参考画像✅

--- WeSeaYou/Assets/Scripts/ActionScene/FieldObj/VIsual/OnGroundVisual.cs
+++ WeSeaYou/Assets/Scripts/ActionScene/FieldObj/VIsual/OnGroundVisual.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class OnGroundVisual : MonoBehaviour
@@ -7,19 +8,16 @@ public class OnGroundVisual : MonoBehaviour
     [SerializeField] GameObject dustLeft;
     [SerializeField] GameObject dustRight;
 
-    public Collider2D col;
+    private Collider2D col;
+    private Rigidbody2D rb;
 
-    private bool isGrounding = true;
+    private bool isGrounding = true;//èâä˙ãÛíÜÇ≈Ç‡trueÅ®falseÇ…Ç»ÇÈÇÃÇ≈ñ‚ëËÇ»Ç¢
     private bool lastIsGrounding = true;
 
     void Start()
     {
-        if (col == null)
-        {
-            col = GetComponentInChildren<Collider2D>();
-        }
-
-        UpdateDustPosition();
+        col = GetComponentInChildren<Collider2D>();
+        rb = col.GetComponentInParent<Rigidbody2D>();
     }
 
     void Update()
@@ -39,6 +37,11 @@ public class OnGroundVisual : MonoBehaviour
 
     void OnGround()
     {
+        if (rb != null && rb.mass >= 7f)
+        {
+            StartDustAnim();
+        }
+
         Vector2 spawnPosition = new Vector2(
             col.bounds.center.x,
             col.bounds.min.y - 0.22f
@@ -56,33 +59,36 @@ public class OnGroundVisual : MonoBehaviour
             particle.gameObject,
             particle.main.duration + particle.main.startLifetime.constantMax
         );
-
-        UpdateDustPosition();
     }
 
-    void UpdateDustPosition()
+    void StartDustAnim()
     {
-        if (col == null)
-        {
-            return;
-        }
+        StartCoroutine(StartDustAnimCoroutine());
+    }
+    IEnumerator StartDustAnimCoroutine()
+    {
+        yield return new WaitForSeconds(0.063f);
 
-        float bottomY = col.bounds.min.y;
+        float bottomY = col.bounds.min.y - 0.1f;
 
         if (dustLeft != null)
         {
-            dustLeft.transform.position = new Vector2(
-                col.bounds.min.x,
+            GameObject left = Instantiate(dustLeft);
+            left.transform.position = new Vector2(
+                col.bounds.min.x - 0.32f,
                 bottomY
             );
+            Destroy(left, 0.7f);
         }
 
         if (dustRight != null)
         {
-            dustRight.transform.position = new Vector2(
-                col.bounds.max.x,
+            GameObject right = Instantiate(dustRight);
+            right.transform.position = new Vector2(
+                col.bounds.max.x + 0.32f,
                 bottomY
             );
+            Destroy(right, 0.7f);
         }
     }
 }
